3f57409220e73d24cd3cfd80dce9ed0247fff198
srushtim16 <84838342+srushtim16@users.noreply.github.com>
2021-05-27 21:47:24 +0530

    Create Hello.php

---
+++ Day1/Hello.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <h1> my first program </h1>
+    </head>
+   <body>
+       <?php 
+            echo "<b> Hello i am Srushti Modi </b>";
+       ?>
+       </body>
+</html>
