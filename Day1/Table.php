70a1e22968eabbe5f3f0949a5809ef9c404a33ab
srushtim16 <84838342+srushtim16@users.noreply.github.com>
2021-05-27 21:54:08 +0530

    Add files via upload

---
+++ Day1/Table.php
@@ -0,0 +1,38 @@
+<html>
+    <body>
+        <br>
+        <table border="1">
+            <tr>
+                <td colspan="3"align="center">HOSTEL</td>
+            </tr>
+            <tr>
+                <td padding:5px>CATEGORY</td>
+                <td>registration Fee</td>
+                <td>Annual/500 hrs whichever is earlier</td>
+            </tr>
+            <tr>
+                <td>students</td>
+                <td>Rs 50/-</td>
+                <td>RS 500</td>
+            </tr>
+            <tr>
+                <td>General</td>
+                <td>Rs 500/-</td>
+                <td>RS 5000</td>
+            </tr>
+            <tr>
+                <td colspan="3"align="center">SERVICE</td>
+            </tr>
+            <tr>
+                <td>students</td>
+                <td>not offered</td>
+                <td>not offered</td>
+            </tr>
+            <tr>
+                <td>General</td>
+                <td>Rs 500</td>
+                <td>RS 1500</td>
+            </tr>
+        </table>
+    </body>
+</html>
