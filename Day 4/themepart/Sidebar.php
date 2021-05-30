15c6ecf337452993d7a13d9566285d9370695aab
srushtim16 <84838342+srushtim16@users.noreply.github.com>
2021-05-30 13:22:58 +0530

    Add files via upload

---
+++ Day 4/themepart/Sidebar.php
@@ -0,0 +1,23 @@
+<div id="sidebar">
+			<ul>
+				<li>
+					<h2>Categories</h2>
+					<ul>
+						<li><a href="#">Indian Places</a></li>
+						<li><a href="#">Dubai Places</a></li>
+						<li><a href="#">Canada Places</a></li>
+						<li><a href="#">U.S. Places</a></li>
+						<li><a href="#">U.K. Places</a></li>
+					</ul>
+				</li>
+				<li>
+					<h2>About us</h2>
+					<ul>
+						<li><a href="#">Successful old tour</a></li>
+						<li><a href="#">Best view place</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+
