e466d05a1014799040f66c4b0ec4479c49eaaf86
srushtim16 <84838342+srushtim16@users.noreply.github.com>
2021-05-30 13:20:34 +0530

    Add files via upload

---
+++ Day 4/Photos.php
@@ -0,0 +1,53 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>TOUR PLANNER</title>
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<?php
+include './themepart/menu.php';
+include './themepart/logo.php';
+?>
+
+<div id="wrapper"> 
+	<!-- end #header -->
+	<div id="page-bgtop"></div>
+	<div id="page">
+            <div><img src="TOUR image.jpg" width="900" height="350" alt="" /></div>
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Photos</a></h2>
+				<div style="clear: both;">&nbsp;</div>
+				<div class="entry">
+                                    <img src="best places.jpg" width="200" height="130" alt="" />
+                                    <img src="dubai.jpg" width="200" height="130" alt="" />
+                                    <img src="canada.jpg" width="200" height="130" alt="" />
+                                    <img src="kerala.jpg" width="200" height="130" alt="" />
+                                    <img src="london.jpg" width="200" height="130" alt="" />
+				</div>
+			</div>
+			<div style="clear: both;">&nbsp;</div>
+		</div>
+		<!-- end #content -->
+                <?php
+                    include './themepart/sidebar.php';
+                 ?>
+				<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<div id="page-bgbtm"></div>
+	<!-- end #page --> 
+</div>
+<?php
+        include './themepart/footer.php';
+?>
+<!-- end #footer -->
+</body>
+</html>
+
