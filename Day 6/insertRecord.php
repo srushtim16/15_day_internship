ef49d7436afa3ddba1ce054e59907a9b377e2543
srushtim16 <84838342+srushtim16@users.noreply.github.com>
2021-06-01 21:33:49 +0530

    Add files via upload

---
+++ Day 6/insertRecord.php
@@ -0,0 +1,32 @@
+<?php
+$connection = mysqli_connect("localhost", "root", "", "db_internship");
+        
+if($_POST){
+    
+    $name = $_POST['txt1'];
+    $gender = $_POST['txt2'];
+    $mobile = $_POST['txt3'];
+    
+    $q = mysqli_query($connection, "insert into tbl_user (user_name,user_gender,user_mobile) values('{$name}','{$gender}','{$mobile}')") or die(mysqli_error($connection));
+    if($q){
+        echo"<script>alter('Record Added')</script>";
+    }
+    
+ }
+
+?>
+
+<html>
+    <body>
+        <form method="POST">
+            Name : <input type="text" name="txt1"/>
+            Gender : <select name="txt2">
+                <option>male</option>
+                <option>female</option>
+            </select>
+            Mobile No : <input type="number" name="txt3"/>
+            <input type="Submit" value="Submit"/>
+        </form>
+    </body>
+    
+</html>
