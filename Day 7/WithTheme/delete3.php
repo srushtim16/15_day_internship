5416c1958e3fb2d93e80285240a5f1504ad88c32
srushtim16 <84838342+srushtim16@users.noreply.github.com>
2021-06-02 21:30:18 +0530

    Create delete3.php

---
+++ Day 7/WithTheme/delete3.php
@@ -0,0 +1,15 @@
+<?php
+$connection = mysqli_connect("localhost", "root", "","db_internship");
+
+$id = $_GET['deleteid'];
+
+$q = mysqli_query($connection,"delete from tbl_student where st_id='{$id}'")
+        or die(mysqli_error($connection));
+
+if($q)
+{
+    echo "<script>alert('Record Deleted');window.location='table3.php';</script>";
+}
+
+
+
